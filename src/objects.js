e8ff095b805957b090e587a45ee49f3b29e19af7
Gregory <huyghegregory@yahoo.fr>
2019-01-22 11:45:57 +0100

    génération aléatoire sur board ok

--- src/objects.js
+++ src/objects.js
@@ -1,14 +1,15 @@
     import {Board, board, ctx, drawBoard} from "./board.js";
 
-    // OBJET JOUEUR
-    function Player(name, sprite, life) {
+    
+    // OBJET OBSTACLE
+    function Obstacle(name, sprite) {
         this.name = name;
         this.sprite = sprite;
-        this.life = life;
     }
-    // INIT OBJET JOUEUR
-    const player1 = new Player("Joueur 1", "assets/joueur1.png", 100);
-    const player2 = new Player("Joueur 2", "assets/joueur2.png", 100);
+    // INIT OBJET OBSTACLE
+    const lava = new Obstacle("Lave", "assets/lave.png");
+
+
 
     // OBJET ARME
     function Weapon(name, sprite, damage) {
@@ -22,24 +23,41 @@
     const axe = new Weapon("Hache", "assets/hache.png", 15);
     const flail = new Weapon("Fléau", "assets/fleau.png", 20);
 
-    // OBJET OBSTACLE
-    function Obstacle(name, sprite) {
+
+
+    // OBJET JOUEUR
+    function Player(name, sprite, life) {
         this.name = name;
         this.sprite = sprite;
+        this.life = life;
     }
-    // INIT OBJET OBSTACLE
-    const lava = new Obstacle("Lave", "assets/lave.png");
+    // INIT OBJET JOUEUR
+    const player1 = new Player("Joueur 1", "assets/joueur1.png", 100);
+    const player2 = new Player("Joueur 2", "assets/joueur2.png", 100);
+
+
+
+    
 
 
     // POSITIONNER UNE PIECE
-    Board.prototype.setPiece = function (piece, x, y) {
-        if (x >= this.width || y >= this.height) {
+    Board.prototype.setPiece = function (piece) {
+        
+        let randomX = Math.floor(Math.random() * board.width);
+        let randomY = Math.floor(Math.random() * board.height);
+        
+        if (randomX >= this.width || randomY >= this.height) {
             throw new Error('Pièce hors limite');
         }
-        this.chartBoard[y][x] = piece;
+        
+        this.chartBoard[randomY][randomX] = piece;
     };
-    const piece1 = board.setPiece(player1, 2, 3);
-    const piece2 = board.setPiece(player2, 4, 8);
+    const piece1 = board.setPiece(player1);
+    const piece2 = board.setPiece(player2);
+
+
+
+
 
     // DESSINER PLAYERS SUR CANVAS
 //    Board.prototype.spawnPlayers = function (piece, x, y) {
